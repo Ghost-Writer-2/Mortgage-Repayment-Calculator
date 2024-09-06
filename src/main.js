e24a36f91b8530db77673d76f7d1251af58780fb
Charles <172846903+Ghost-Writer-2@users.noreply.github.com>
2024-09-06 21:48:17 +0100

    Cleaned the code and corrected the attribution

--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
-(function(){
-  const FORM = document.querySelector('.js-form');
-  const NUMBER_INPUTS = Array.from(document.querySelectorAll('.js-number-input'));
-  const RADIO_BTN = Array.from(document.querySelectorAll('.radio-input'));
-  const results = document.querySelector('.card--result');
-  const clear_all = document.querySelector('.js-clear-all');
+(function () {
+  const FORM = document.querySelector(".js-form");
+  const NUMBER_INPUTS = Array.from(
+    document.querySelectorAll(".js-number-input")
+  );
+  const RADIO_BTN = Array.from(document.querySelectorAll(".radio-input"));
+  const results = document.querySelector(".card--result");
+  const clear_all = document.querySelector(".js-clear-all");
 
-//  removes default html validation information
-  document.addEventListener("invalid", (e) => {
-    e.preventDefault();
-    showError();
-  }, true)
+  //  removes default html validation information
+  document.addEventListener(
+    "invalid",
+    (e) => {
+      e.preventDefault();
+      showError();
+    },
+    true
+  );
 
   /************************************** form validation section ***************************/
-  FORM.addEventListener('submit', (e) => {
+  FORM.addEventListener("submit", (e) => {
     e.preventDefault();
 
     if (!FORM.checkValidity()) {
@@ -20,46 +26,45 @@
     } else {
       displayResult();
     }
-  })
+  });
 
   // removes error if valid
   NUMBER_INPUTS.forEach((input) => {
     const error_text = document.querySelector(`.${input.id}-error-text`);
     const span = document.querySelector(`.${input.id}-js`);
 
-    input.addEventListener('input', () => {
+    input.addEventListener("input", () => {
       if (input.validity.valid) {
-        error_text.classList.add('hide');
+        error_text.classList.add("hide");
         span.style.backgroundColor = "";
         span.style.color = "";
         span.style.borderColor = "";
-        input.style.border = '';
+        input.style.border = "";
       }
-    })
-  })
+    });
+  });
 
   RADIO_BTN.forEach((input) => {
     const error_text = document.querySelector(`.repayment-error-text`);
 
-    input.addEventListener('input', () => {
+    input.addEventListener("input", () => {
       if (input.validity.valid) {
-        error_text.classList.add('hide');
+        error_text.classList.add("hide");
       }
 
-      green(input)
-    })
-  })
+      green(input);
+    });
+  });
 
   /****************************************** error function *****************************/
   function showError() {
-
     // number inputs
     NUMBER_INPUTS.forEach((input) => {
       const error_text = document.querySelector(`.${input.id}-error-text`);
       const span = document.querySelector(`.${input.id}-js`);
 
-      function error(message = 'This field is required') {
-        error_text.classList.remove('hide');
+      function error(message = "This field is required") {
+        error_text.classList.remove("hide");
         input.style.border = "1px solid hsl(4, 69%, 50%)";
         span.style.backgroundColor = "hsl(4, 69%, 50%)";
         span.style.color = "hsl(0, 0%, 100%)";
@@ -72,21 +77,22 @@
       }
 
       if (input.validity.rangeOverflow || input.validity.rangeUnderflow) {
-        error(`Please make sure the value is within ${input.min} - ${input.max}`)
+        error(
+          `Please make sure the value is within ${input.min} - ${input.max}`
+        );
       }
-    })
+    });
 
     // radio buttons
     RADIO_BTN.forEach((input) => {
       const error_text = document.querySelector(`.repayment-error-text`);
 
       if (input.validity.valueMissing) {
-        error_text.classList.remove('hide');
-        error_text.textContent = 'This field is required';
+        error_text.classList.remove("hide");
+        error_text.textContent = "This field is required";
       }
-    })
+    });
   }
-
 
   /*************************************** green thingy ***************************************/
   function green(element) {
@@ -94,14 +100,14 @@
     const repayment = document.querySelector(`.js-${RADIO_BTN[0].id}`);
     const interest = document.querySelector(`.js-${RADIO_BTN[1].id}`);
 
-    label.classList.add('green');
-    
-    if (repayment.classList.contains('green') && element === RADIO_BTN[0]) {
-      interest.classList.remove('green');
-    } 
+    label.classList.add("green");
 
-    if (interest.classList.contains('green') && element === RADIO_BTN[1]) {
-      repayment.classList.remove('green');
+    if (repayment.classList.contains("green") && element === RADIO_BTN[0]) {
+      interest.classList.remove("green");
+    }
+
+    if (interest.classList.contains("green") && element === RADIO_BTN[1]) {
+      repayment.classList.remove("green");
     }
   }
 
@@ -113,11 +119,10 @@
 
     var monthlyRepayment = numberWithCommas(repayment());
     var interest = numberWithCommas(interestOnly());
-    var totalRepayment = numberWithCommas(totalPayment())
+    var totalRepayment = numberWithCommas(totalPayment());
 
     if (RADIO_BTN[0].checked) {
-      results.innerHTML = 
-      `
+      results.innerHTML = `
          <div class="card--result__filled">
           <p class="title">Your results</p>
           <p class="info">
@@ -134,10 +139,9 @@
           </div>
         </div>
 
-      `
+      `;
     } else if (RADIO_BTN[1].checked) {
-      results.innerHTML = 
-      `
+      results.innerHTML = `
          <div class="card--result__filled">
           <p class="title">Your results</p>
           <p class="info">
@@ -154,36 +158,37 @@
           </div>
         </div>
 
-      `
+      `;
     }
   }
 
   /**************************************************** calculation functions  *********************************************/
   var calculate = {
-
-    interestOnly: function() {
+    interestOnly: function () {
       var r = NUMBER_INPUTS[2].value / 100;
       var p = NUMBER_INPUTS[0].value;
-      
-      return parseFloat(((r * p) / 12) .toFixed(2));
+
+      return parseFloat(((r * p) / 12).toFixed(2));
     },
 
-    repayment: function() {
+    repayment: function () {
       const p = parseFloat(NUMBER_INPUTS[0].value);
-      const r = parseFloat( ((NUMBER_INPUTS[2].value) / 100) / 12 );
+      const r = parseFloat(NUMBER_INPUTS[2].value / 100 / 12);
       const n = parseFloat(NUMBER_INPUTS[1].value * 12);
 
-      var firstHalf = parseFloat( (Math.pow((1 + r), n) * r) );
-      var secondHalf = parseFloat( (Math.pow((1 + r), n) - 1) );
-      var finalHalf = parseFloat( (p * (firstHalf / secondHalf)) .toFixed(2) );
+      var firstHalf = parseFloat(Math.pow(1 + r, n) * r);
+      var secondHalf = parseFloat(Math.pow(1 + r, n) - 1);
+      var finalHalf = parseFloat((p * (firstHalf / secondHalf)).toFixed(2));
 
-      return parseFloat(finalHalf .toFixed(2));
+      return parseFloat(finalHalf.toFixed(2));
     },
 
-    totalPayment: function() {
-      return parseFloat((calculate.repayment() * (NUMBER_INPUTS[1].value * 12)) .toFixed(2));
-    }
-  }
+    totalPayment: function () {
+      return parseFloat(
+        (calculate.repayment() * (NUMBER_INPUTS[1].value * 12)).toFixed(2)
+      );
+    },
+  };
 
   var interestOnly = calculate.interestOnly;
   var repayment = calculate.repayment;
@@ -191,18 +196,17 @@
 
   /***************************************************clear all inputs ******************************************/
   clear_all.addEventListener("click", () => {
-    NUMBER_INPUTS.forEach(input => {
-      input.value = '';
-    })
+    NUMBER_INPUTS.forEach((input) => {
+      input.value = "";
+    });
 
-    RADIO_BTN.forEach(input => {
+    RADIO_BTN.forEach((input) => {
       input.checked = false;
       const label = document.querySelector(`.js-${input.id}`);
-      label.classList.remove('green');
-    })
+      label.classList.remove("green");
+    });
 
-    results.innerHTML = 
-    `
+    results.innerHTML = `
        <div class="card--result__empty">
           <img src="/assets/images/illustration-empty.svg" alt="" />
           <h2>Results shown here</h2>
@@ -211,6 +215,6 @@
             monthly repayments would be.
           </p>
         </div>    
-    `
-  })
-})()
+    `;
+  });
+})();
